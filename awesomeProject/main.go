c586c1ee68b0654d6cb2aa240abaea412db3055f
jungwook <mr4454@naver.com>
2025-11-04 21:21:00 +0900

    feat(getTodoById): Implement getTodoById
    implement getTodoById with gin:
    - router, , := , err!=nil, gin H, errors New, for &, *

--- awesomeProject/main.go
+++ awesomeProject/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -36,9 +37,40 @@ func addTodo(context *gin.Context) {
 	context.IndentedJSON(http.StatusCreated, newTodo)
 }
 
+// (param) (return this, or error)
+func getTodoById(id string) (*todo, error) {
+	// for index, value := slice
+	for index, t := range todos {
+		if t.ID == id {
+			//return value, and error which is nil
+			return &todos[index], nil
+		}
+	}
+
+	return nil, errors.New("todo not found")
+}
+
+// front *behind mean front is behind's pointer
+// go's return default setting is void don't need to write void or ()
+// if curly bracket is one, it means param
+func getTodo(context *gin.Context) {
+	id := context.Param("id")
+	//todo or error will get
+	todo, err := getTodoById(id)
+	//go doesn't use curly brackets in if condition
+	if err != nil {
+		//HashMap by gin
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not Found"})
+		return
+	}
+
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
+	router.GET("/todos/:id", getTodo)
 	router.POST("/todos", addTodo)
 	router.Run("localhost:9090")
 }
