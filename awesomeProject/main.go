a609951586db4cf9bc6a3cf5f60492e75c0d8128
jungwook <mr4454@naver.com>
2025-11-04 20:01:40 +0900

    feat(getTodos): implement getTodos
    Implement getTodos with gin:
    - router, context, IndentedJson, http are used

--- awesomeProject/main.go
+++ awesomeProject/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID        string `json: "id"`
+	Item      string `json: "title"`
+	Completed bool   `json: "completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Clean Room", Completed: false},
+	{ID: "2", Item: "Read Book", Completed: false},
+	{ID: "3", Item: "Record my movie", Completed: false},
+}
+
+func getTodos(context *gin.Context) { //context is all things from request
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.Run("localhost:9090")
 }
