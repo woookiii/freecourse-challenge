5f09e4107b54c86c93380dbde72143a6e9dae0ca
jungwook <mr4454@naver.com>
2025-11-04 20:43:52 +0900

    feat(addTodo): implement addTodo
    Implement addTodo with gin:
    - :=;, BindJSON, nil, slice, append, IndentedJson, http StatusCreated

--- awesomeProject/main.go
+++ awesomeProject/main.go
@@ -8,8 +8,8 @@ import (
 
 type todo struct {
 	ID        string `json: "id"`
-	Item      string `json: "title"`
-	Completed bool   `json: "completed"`
+	Item      string `json: "item"`
+	Completed bool   `json: "completed"` //this make struct to json
 }
 
 var todos = []todo{
@@ -18,12 +18,27 @@ var todos = []todo{
 	{ID: "3", Item: "Record my movie", Completed: false},
 }
 
-func getTodos(context *gin.Context) { //context is all things from request
+func getTodos(context *gin.Context) { //context is all things from request, response
 	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func addTodo(context *gin.Context) {
+	var newTodo todo
+
+	//declare initialize at the same time and if context is not fit on newTodo it will throw error
+	//if it fit, newTodo get json
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodo)
+
+	context.IndentedJSON(http.StatusCreated, newTodo)
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
+	router.POST("/todos", addTodo)
 	router.Run("localhost:9090")
 }
