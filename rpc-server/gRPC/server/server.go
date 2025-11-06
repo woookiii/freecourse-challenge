7e455848183e9e7bdca700f82a4d1325e4e95092
jungwook <mr4454@naver.com>
2025-11-06 11:16:36 +0900

    feat(server): implement server and methods
    Implement server and requiring methods for AuthServiceServer:
    - server verify token and response
    - client make token and send it to server and server save it

---
+++ rpc-server/gRPC/server/server.go
@@ -0,0 +1,75 @@
+package server
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"net"
+	"rpc-server/config"
+	"rpc-server/gRPC/paseto"
+	auth "rpc-server/gRPC/proto"
+)
+
+type GRPCServer struct {
+	auth.AuthServiceServer
+	pasetoMaker    *paseto.PasetoMaker
+	tokenVerifyMap map[string]*auth.AuthData
+}
+
+func NEWGRPCServer(cfg *config.Config) error {
+	if lis, err := net.Listen("tcp", cfg.GRPC.URL); err != nil {
+		return err
+	} else {
+
+		server := grpc.NewServer([]grpc.ServerOption{}...)
+
+		auth.RegisterAuthServiceServer(server, &GRPCServer{
+			pasetoMaker:    paseto.NewPasetoMaker(cfg),
+			tokenVerifyMap: make(map[string]*auth.AuthData),
+		})
+		//register server we will use
+		reflection.Register(server)
+
+		go func() {
+			log.Println("Start grpc server")
+			if err = server.Serve(lis); err != nil {
+				panic(err)
+			}
+		}()
+		//if Serve is run, the codes after it can't run in same thread, so we Serve server in new thread through go routine
+	}
+
+	return nil
+}
+
+func (s *GRPCServer) CreateAuth(_ context.Context, req *auth.CreateTokenReq) (*auth.CreateTokenRes, error) {
+	data := req.Auth
+	token := data.Token
+
+	s.tokenVerifyMap[token] = data
+
+	return &auth.CreateTokenRes{Auth: data}, nil
+}
+
+func (s *GRPCServer) VerifyAuth(_ context.Context, req *auth.VerifyTokenReq) (*auth.VerifyTokenRes, error) {
+	token := req.Token
+
+	res := &auth.VerifyTokenRes{V: &auth.Verify{
+		Auth: nil,
+	}}
+
+	if authData, ok := s.tokenVerifyMap[token]; !ok {
+		res.V.Status = auth.ResponseType_FAILED
+	} else if authData.ExpireDate < time.Now().Unix() {
+		delete(s.tokenVerifyMap, token)
+		res.V.Status = auth.ResponseType_EXPIRED_DATE
+	} else {
+		res.V.Status = auth.ResponseType_SUCCESS
+	}
+
+	return res, nil
+}
