ea33aa7e7ed910cb6d786c22752dbbc5663b4c48
jungwook <mr4454@naver.com>
2025-11-19 01:37:31 +0900

    feat(service): delete unneccessary retry logic
    Delete unneccessary retry logic:
    - error will pop from repository well now
    
    Break goto retry logic deleted

--- project/api/module/API/service/member.go
+++ project/api/module/API/service/member.go
@@ -13,28 +13,17 @@ func (s *Service) CreateMember(req *dto.MemberSaveReq) error {
 		return nil
 	} else if hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost); err != nil {
 		return err
+	} else if err := s.repository.CreateMember(
+		req.Name,
+		req.Email,
+		string(hashedPassword),
+	); err != nil {
+		log.Println("Failed to create member", "Member name", req.Name, "err", err.Error())
+		return err
 	} else {
-		retryCount := 0
-	createAgain:
-		if s.repository.CreateMember(
-			req.Name,
-			req.Email,
-			string(hashedPassword),
-		); err != nil {
-			retryCount++
-
-			if retryCount < 3 {
-				goto createAgain
-			} else {
-				log.Println("Failed to create member", "Member name", req.Name, "err", err.Error())
-				return err
-			}
-		} else {
-			log.Println("Success create new member", "Member name", req.Name)
-			//TODO produce message to kafka in another goroutine
-			return nil
-		}
-
+		log.Println("Success create new member", "Member name", req.Name)
+		//TODO produce message to kafka in another goroutine
+		return nil
 	}
 
 }
