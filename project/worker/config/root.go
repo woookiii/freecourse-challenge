af8231547f795f3a0673e951701a8f6849ff6c34
jungwook <mr4454@naver.com>
2025-11-21 11:23:04 +0900

    feat: Newconfig for worker server
    We start with kafka and db

---
+++ project/worker/config/root.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"os"
+
+	"github.com/naoina/toml"
+)
+
+type Config struct {
+	DB struct {
+		Database string
+		URL      string
+	} `toml:"db"`
+
+	Kafka struct {
+		URLS []string
+	} `toml:"kafka"`
+}
+
+func NewConfig(path string) *Config {
+	c := new(Config)
+
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	err = toml.NewDecoder(f).Decode(c)
+	if err != nil {
+		panic(err)
+	}
+
+	return c
+}
