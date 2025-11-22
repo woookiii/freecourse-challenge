934f92d5d4e16cf1edf884f25ff35f7a24d94ba0
jungwook <mr4454@naver.com>
2025-11-23 00:20:01 +0900

    feat(kafka): implement consumer logic and handler
    Same as worker for replica db, service logic will
    be different

---
+++ project/worker-search/kafka/handler.go
@@ -0,0 +1,39 @@
+package kafka
+
+import (
+	"encoding/json"
+	"log"
+	"worker/module/connector/entity"
+
+	"github.com/IBM/sarama"
+)
+
+func (k *Kafka) Setup(_ sarama.ConsumerGroupSession) error {
+	close(k.ready)
+	return nil
+}
+
+func (k *Kafka) Cleanup(_ sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (k *Kafka) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for {
+		select {
+		case msg := <-claim.Messages():
+			session.MarkMessage(msg, "")
+			var member entity.Member
+			if err := json.Unmarshal(msg.Value, &member); err != nil {
+				log.Printf("Fail to unmarshal msg value to member struct: %v", err)
+				continue
+			}
+			err := k.service.SaveMember(&member)
+			if err != nil {
+				log.Printf("Fail to save member to replica db: %v", err)
+				continue
+			}
+		case <-session.Context().Done():
+			return nil
+		}
+	}
+}
