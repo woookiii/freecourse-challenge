56455d1a03f77d274cf79c97ce82a98673019610
jungwook <mr4454@naver.com>
2025-11-23 00:09:26 +0900

    feat: implement NewConfig for worker-search
    worker-search will create doc at member index for
    search db

---
+++ project/worker-search/config/root.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"os"
+
+	"github.com/naoina/toml"
+)
+
+type Config struct {
+	Kafka struct {
+		URLS    []string
+		GroupID string
+	} `toml:"kafka"`
+}
+
+func NewConfig(path string) *Config {
+	c := new(Config)
+
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	err = toml.NewDecoder(f).Decode(c)
+	if err != nil {
+		panic(err)
+	}
+
+	return c
+}
---
+++ project/worker-search/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"worker-search/config"
+
+	"go.uber.org/fx"
+)
+
+var cfgPath = flag.String("cfg", "./config.toml", "config path")
+
+func main() {
+	flag.Parse()
+
+	cfg := config.NewConfig(*cfgPath)
+
+	fx.New(
+		fx.Provide(func() *config.Config { return cfg }),
+	).Run()
+}
